ccedb460b57b2c653399951d7f02211f113d8af6
DBuraz <78876576+DBuraz@users.noreply.github.com>
2021-10-14 13:54:28 +0200

    Loaded file

--- 2_code/TeamRainbow.R
+++ 2_code/TeamRainbow.R
@@ -1,2 +1,9 @@
 ### CDS Diversity Hackathon #####
 ### Script to visualize diversity gaps over time #####
+
+
+#loading packages
+library(tidyverse)
+
+#loading file
+diversity <- read.csv("1_data/unibas_diversity.csv")
