aa773ce99ce14866783bf7a0ae30da460a42e7ea
TisdallL <44674483+TisdallL@users.noreply.github.com>
2021-10-14 14:08:30 +0200

    Update TeamRainbow.R

--- 2_code/TeamRainbow.R
+++ 2_code/TeamRainbow.R
@@ -1,2 +1,15 @@
 ### CDS Diversity Hackathon #####
 ### Script to visualize diversity gaps over time #####
+
+getwd()
+# load data
+div_dat <- read.csv('1_data/unibas_diversity.csv')
+head(div_dat)
+View(div_dat)
+
+### AGE GAP
+plot(div_dat$age_first)
+
+### GENDER GAP
+
+### NATIONALITY GAP
