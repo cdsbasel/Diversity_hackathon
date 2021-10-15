684b12068610f5908e070b8abaa0e0d87ac1b76d
Samuel Aeschbach <samuel.aeschbach@unibas.ch>
2021-10-15 14:16:11 +0200

    finish barebone task 1

--- 2_code/PARASAM_Task_1.r
+++ 2_code/PARASAM_Task_1.r
@@ -5,12 +5,10 @@ library(tidyverse)
 data <- read_csv("1_data/unibas_diversity.csv")
 
 # gender proportion over time
-
-
 data %>% 
   select(year, gender_proportion) %>% 
   ggplot(aes(x = year, y = gender_proportion)) +
-  geom_jitter(alpha = 0.1)
+  geom_jitter(width = 5, alpha = 0.1)
 
 data %>% 
   select(year, gender_proportion) %>% 
@@ -18,3 +16,26 @@ data %>%
   summarize(prop_fem = mean(gender_proportion, na.rm = TRUE)) %>% 
   ggplot(aes(x = year, y = prop_fem)) +
   geom_point()
+
+# age over time
+data %>% 
+  select(year, age_average) %>% 
+  group_by(year) %>% 
+  summarize(age = mean(age_average, na.rm = TRUE)) %>% 
+  ggplot(aes(x = year, y = age)) +
+  geom_point()
+
+# nationality over time
+data %>% 
+  select(year, nation_proportion) %>% 
+  group_by(year) %>% 
+  summarize(western = mean(nation_proportion, na.rm = TRUE)) %>% 
+  ggplot(aes(x = year, y = western)) +
+  geom_point()
+
+
+
+
+
+
+
